92c7e9222a288f009caecccf179eec21a61c59d0
mahfuj926 <mahfujrahman.jewel@gmail.com>
2024-07-14 05:39:33 +0600

    create a new blog collection, stored data in database and get the data

--- index.js
+++ index.js
@@ -24,14 +24,32 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
+    // await client.connect();
+    
+    // database collections
+    const blogCollection = client.db('popularDB').collection('blog')
+
+
+    // post and get newBlog
+    app.post("/blogs", async (req, res) =>{
+        const newBlog = req.body;
+        const result = await blogCollection.insertOne(newBlog);
+        res.send(result);
+    });
+
+    app.get('/blogs', async(req,res) =>{
+        const result = await blogCollection.find().toArray();
+        res.send(result);
+    })
+
+
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
